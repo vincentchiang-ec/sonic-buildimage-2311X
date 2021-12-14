6da8577213aadbc75d863ec32aae7d0dcfefa9e0
Sean Wu <sean_wu@edge-core.com>
2021-12-14 03:13:43 +0000

    Add serial num sysfs node to PSU driver
    * Model: AS7726-32X, AS7326-56X
    
    Signed-off-by: Sean Wu <sean_wu@edge-core.com>

--- platform/broadcom/sonic-platform-modules-accton/as7326-56x/modules/accton_as7326_56x_psu.c
+++ platform/broadcom/sonic-platform-modules-accton/as7326-56x/modules/accton_as7326_56x_psu.c
@@ -35,6 +35,7 @@
 #include <linux/dmi.h>
 
 #define MAX_MODEL_NAME          16
+#define MAX_SERIAL_NUMBER       18
 static ssize_t show_status(struct device *dev, struct device_attribute *da, char *buf);
 static ssize_t show_string(struct device *dev, struct device_attribute *da, char *buf);
 static int as7326_56x_psu_read_block(struct i2c_client *client, u8 command, u8 *data,int data_len);
@@ -54,6 +55,7 @@ struct as7326_56x_psu_data {
     u8  index;           /* PSU index */
     u8  status;          /* Status(present/power_good) register read from CPLD */
     char model_name[MAX_MODEL_NAME+1]; /* Model name, read from eeprom */
+    char serial_number[MAX_SERIAL_NUMBER+1];
 };
 
 static struct as7326_56x_psu_data *as7326_56x_psu_update_device(struct device *dev);
@@ -61,6 +63,7 @@ static struct as7326_56x_psu_data *as7326_56x_psu_update_device(struct device *d
 enum as7326_56x_psu_sysfs_attributes {
     PSU_PRESENT,
     PSU_MODEL_NAME,
+    PSU_SERIAL_NUMBER,
     PSU_POWER_GOOD
 };
 
@@ -68,11 +71,13 @@ enum as7326_56x_psu_sysfs_attributes {
  */
 static SENSOR_DEVICE_ATTR(psu_present,    S_IRUGO, show_status,    NULL, PSU_PRESENT);
 static SENSOR_DEVICE_ATTR(psu_model_name, S_IRUGO, show_string,    NULL, PSU_MODEL_NAME);
+static SENSOR_DEVICE_ATTR(psu_serial_number, S_IRUGO, show_string,    NULL, PSU_SERIAL_NUMBER);
 static SENSOR_DEVICE_ATTR(psu_power_good, S_IRUGO, show_status,    NULL, PSU_POWER_GOOD);
 
 static struct attribute *as7326_56x_psu_attributes[] = {
     &sensor_dev_attr_psu_present.dev_attr.attr,
     &sensor_dev_attr_psu_model_name.dev_attr.attr,
+    &sensor_dev_attr_psu_serial_number.dev_attr.attr,
     &sensor_dev_attr_psu_power_good.dev_attr.attr,
     NULL
 };
@@ -111,6 +116,9 @@ static ssize_t show_string(struct device *dev, struct device_attribute *da,
 
     if (attr->index == PSU_MODEL_NAME) {
         ptr = data->model_name;
+    }
+    else if (attr->index == PSU_SERIAL_NUMBER) {
+        ptr = data->serial_number;
     }
     else {
         return -EIO;
@@ -251,12 +259,27 @@ struct model_name_info {
     char* model_name;
 };
 
+struct serial_number_info {
+    enum psu_type type;
+    u8 offset;
+    u8 length;
+    char* serial_number;
+};
+
 struct model_name_info models[] = {
     {PSU_TYPE_AC_110V, 0x20, 8, "YM-2651Y"},
     {PSU_TYPE_DC_48V,  0x20, 8, "YM-2651V"},
     {PSU_TYPE_DC_12V,  0x00, 11, "PSU-12V-750"},
     {PSU_TYPE_AC_ACBEL_FSF019, 0x15, 10, "FSF019-"},
     {PSU_TYPE_AC_ACBEL_FSF045, 0x15, 10, "FSF045-"}
+};
+
+struct serial_number_info serials[] = {
+    {PSU_TYPE_AC_110V, 0x2e, 18, "YM-2651Y"},
+    {PSU_TYPE_DC_48V,  0x2e, 18, "YM-2651V"},
+    {PSU_TYPE_DC_12V,  0x2e, 18, "PSU-12V-750"},
+    {PSU_TYPE_AC_ACBEL_FSF019, 0x2e, 16, "FSF019-"},
+    {PSU_TYPE_AC_ACBEL_FSF045, 0x2e, 16, "FSF045-"}
 };
 
 static int as7326_56x_psu_model_name_get(struct device *dev)
@@ -293,6 +316,40 @@ static int as7326_56x_psu_model_name_get(struct device *dev)
     return -ENODATA;
 }
 
+static int as7326_56x_psu_serial_number_get(struct device *dev)
+{
+    struct i2c_client *client = to_i2c_client(dev);
+    struct as7326_56x_psu_data *data = i2c_get_clientdata(client);
+    int i, status;
+
+    for (i = 0; i < ARRAY_SIZE(models); i++) {
+        memset(data->serial_number, 0, sizeof(data->serial_number));
+
+        status = as7326_56x_psu_read_block(client, serials[i].offset,
+                                           data->serial_number, serials[i].length);
+        if (status < 0) {
+            data->serial_number[0] = '\0';
+            dev_dbg(&client->dev, "unable to read serial_number from (0x%x) offset(0x%x)\n",
+                                  client->addr, serials[i].offset);
+            return status;
+        }
+        else {
+            data->serial_number[serials[i].length] = '\0';
+        }
+
+        /* Determine if the model name is known, if not, read next index
+         */
+        if (strncmp(data->model_name, models[i].model_name, strlen(models[i].model_name)) == 0) {
+            return 0;
+        }
+        else {
+            data->serial_number[0] = '\0';
+        }
+    }
+
+    return -ENODATA;
+}
+
 static struct as7326_56x_psu_data *as7326_56x_psu_update_device(struct device *dev)
 {
     struct i2c_client *client = to_i2c_client(dev);
@@ -321,10 +378,14 @@ static struct as7326_56x_psu_data *as7326_56x_psu_update_device(struct device *d
 
         /* Read model name */
         memset(data->model_name, 0, sizeof(data->model_name));
+        memset(data->serial_number, 0, sizeof(data->serial_number));
         power_good = (data->status >> (3-data->index) & 0x1);
 
         if (power_good) {
             if (as7326_56x_psu_model_name_get(dev) < 0) {
+                goto exit;
+            }
+            if (as7326_56x_psu_serial_number_get(dev) < 0) {
                 goto exit;
             }
         }
